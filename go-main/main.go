4ad593b4bf27650a08d48afed5dacbdf43e1c013
Deivison Nogueira <deivison.nogueira@live.com>
2022-11-26 19:39:38 -0400

    initial work

---
+++ go-main/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"go-main/model"
+)
+
+func main() {
+	http.HandleFunc("/alunos", func(resp http.ResponseWriter, req *http.Request) {
+		if req.Method == "POST" {
+
+			var aluno model.Aluno
+			err := json.NewDecoder(req.Body).Decode(&aluno)
+			if err != nil {
+				fmt.Printf("Desculpe, Não consegui Ler :( : %s", err.Error())
+				http.Error(resp, "Erro  ao Criar Dado, la ele", http.StatusBadRequest)
+				return
+			}
+
+			if aluno.ID <= 0 {
+				http.Error(resp, "Error", http.StatusBadRequest)
+				return
+			}
+
+			resp.WriteHeader(http.StatusCreated)
+			return
+		}
+
+		http.Error(resp, "Error", http.StatusBadRequest)
+
+	})
+}
---
+++ go-main/model/alunoModel.go
@@ -0,0 +1,8 @@
+package model
+
+type Aluno struct {
+	ID        int     `json: "id"`
+	NomeAluno string  `json:"nome"`
+	Atividade string  `json: "atividade"`
+	NotaAluno float32 `json: "notas"`
+}
